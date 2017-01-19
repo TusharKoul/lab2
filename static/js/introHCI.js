82f23d8bbbf30a14869685080380dbdeca55a3c8
tusharkoul <tusharkoul@gmail.com>
2017-01-18 21:23:28 -0800

    adding debugging code

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -20,9 +20,19 @@ function initializePage() {
 }
 
 function projectClick(e) {
+    console.log('project clicked!');
     // prevent the page from reloading 
     e.preventDefault();
     // In an event handler, $(this) refers to 
     // the object that triggered the event 
     $(this).css("background-color", "#7fff00");
+
+    var projectTitle = $(this).find('p').text();
+
+    console.log('projectTitle is ' + projectTitle);
+
+    var jumbotronHeader = $(".jumbotron h1");
+    console.log(jumbotronHeader.text());
+    jumbotronHeader.text(projectTitle);
+
 }
